122778efc5dec00854f07ce7af24e89da59be379
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-09-09 06:44:35 +0200

    Project Initialization

---
+++ src/test/java/com/dgmf/HibernateOneToManyAnnotationSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateOneToManyAnnotationSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
